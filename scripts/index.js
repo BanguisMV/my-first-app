7fee051212e556b214635bc11c2b8411361ce88c
Marky <MarkVergelBanguis@gmail.com>
2020-05-29 23:09:40 -0700

    some changes

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,17 @@ const save = document.querySelector('#save-button');
 const clear = document.querySelector('#clear');
 const remove = document.querySelector('#remove');
 const reverse = document.querySelector('#reverse-button');
-const currentDate = document.querySelector('#currentDate');
+const switchButton = document.querySelector('#switch-button');
+const buttons = document.querySelectorAll('.button-svg');
+const header = document.querySelector('.header');
+const date = document.querySelector('.date-now');
+const container = document.querySelector('.container');
+
 
 //UI.updateTask(3);
 //Page will display if there is an existing Task.
 window.addEventListener('DOMContentLoaded', UI.displayTask);
-window.addEventListener('DOMContentLoaded',UI.getCurrentDate());
+window.addEventListener('DOMContentLoaded', UI.getCurrentDate());
 
 
 // Event Listeners
@@ -19,7 +24,18 @@ cancel.addEventListener('click', () => form.style.display = 'none');
 save.addEventListener('click', UI.submitTask);
 clear.addEventListener('click', Storage.clearFields);
 reverse.addEventListener('click', UI.toggle)
+switchButton.addEventListener('click', UI.changeColor);
 
+
+
+
+
+
+
+
+
+
+//Remove
 window.addEventListener('click', function (e) {
     if (e.target.classList.contains('fa-times')) {
         const delIndex = parseInt(e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
@@ -39,8 +55,9 @@ window.addEventListener('click', function (e) {
     }
 });
 
+//Check if Done
 window.addEventListener('click', function (e) {
-    if (e.target.classList.contains('fa-pencil')) {
+    if (e.target.classList.contains('fa-check')) {
         const delIndex = parseInt(e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
         const task = Storage.getTasks();
         if (task.length === 0) {
@@ -57,4 +74,3 @@ window.addEventListener('click', function (e) {
 
     }
 });
-
